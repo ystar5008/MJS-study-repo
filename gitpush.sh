5e95bfd148ef9ba4347c8e53b274f830c1caa352
ystar5008@naver.com <ystar5008@naver.com>
2024-01-20 00:12:29 +0900

    코드팩토리 강의

---
+++ gitpush.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#실행 sh gitspush.sh
+start_time=$(date +%s)
+end_time=$(date +%s)
+echo "깃커밋을 실행합니다.$start_time"
+git add . 
+git commit -m "코드팩토리 강의"
+git push origin master
+echo "깃커밋을 종료합니다.$end_time
