983d79675e97842324407da0ca37d551a1ff10a3
unknown <ystar5008@naver.com>
2024-01-23 17:26:18 +0900

    자바스크립트 올인원

--- gitpush.sh
+++ gitpush.sh
@@ -2,9 +2,18 @@
 #실행 sh gitspush.sh
 start_time=$(date +%s)
 end_time=$(date +%s)
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+target_branch="NoteBookBranch"
+echo "현재 브랜치 이름"
+if [ "$current_branch" = "main" ]; then
+    git checkout $target_branch
+    continue
+fi
 echo "깃커밋을 실행합니다.$start_time"
 git pull origin main
 git add . 
 git commit -m "자바스크립트 올인원"
-git push origin main
+git checkout main
+git pull origin main
+git push origin NoteBookBranch
 echo "깃커밋을 종료합니다.$end_time
