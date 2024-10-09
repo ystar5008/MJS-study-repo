c294b6d68eea48c9edd29abdb7bf1d6ebafc8284
index.ys <ystar5008@naver.com>
2024-10-10 01:47:18 +0900

    .

--- Data-Structure/string/문자열.js
+++ Data-Structure/string/문자열.js
@@ -3,10 +3,12 @@ class CustomString {
         this.str = str
     }
 
+    //문자열의 길이 반환
     length() {
         return this.str.length
     }
 
+    //문자열 초기화
     append(text) {
         this.str += text
     }
@@ -16,14 +18,18 @@ class CustomString {
         this.str = this.str.split(char).join("")
     }
 
+
+    //문자열에서 특정문자 찾기
     find(substring) {
         return this.str.indexOf(substring)
     }
 
+    //문자열 반환
     get() {
         return this.str
     }
 
+    //문자열 초기화
     clear() {
         this.str = ""
     }
@@ -33,3 +39,10 @@ const customString = new CustomString("안녕")
 console.log(customString.get())
 customString.append("하세요")
 console.log(customString.get())
+console.log(customString.length())
+console.log(customString.find("하"))
+console.log(customString.get().split(""))
+console.log(customString.remove("하"))
+console.log(customString.get())
+
+
